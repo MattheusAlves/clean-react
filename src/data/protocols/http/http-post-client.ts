3e0341665ce5edccf0d0d1a0359f1c2c49883811
MattheusAlves <matheus_alves0101@outlook.com>
2021-03-25 02:38:27 -0300

    feat: ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,7 +1,11 @@
+import { HttpResponse } from "./http-response";
+
 export type HttpPostParams = {
   url: string
   body?: Record<string, any>
 }
+
+
 export interface HttpPostClient {
-  post(params: HttpPostParams): Promise<void>
+  post(params: HttpPostParams): Promise<HttpResponse>
 }
